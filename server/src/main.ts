8d0335296979baa262a0971d6cf5ba5549a8e60f
shadeofgod <zoubingwu@gmail.com>
2019-08-31 04:56:12 +0800

    add swagger module

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-    const app = await NestFactory.create(AppModule);
-    await app.listen(3000);
+  const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api');
+
+  const options = new DocumentBuilder()
+    .setTitle('Paperin API documentation')
+    .setVersion('1.0')
+    .setBasePath('api')
+    .build();
+
+  const doc = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('/docs', app, doc);
+
+  await app.listen(3000);
 }
 bootstrap();
