6eaf677db4cc57c8e32ac262bac091f19e0c7f64
shadeofgod <zoubingwu@gmail.com>
2019-09-01 05:54:59 +0800

    serve static in app module

--- packages/server/src/app.module.ts
+++ packages/server/src/app.module.ts
@@ -1,10 +1,25 @@
+import * as path from 'path';
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ServeStaticModule } from '@nestjs/serve-static';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserEntity } from './user/user.entity';
 
 @Module({
-    imports: [],
-    controllers: [AppController],
-    providers: [AppService],
+  imports: [
+    ServeStaticModule.forRoot({
+      rootPath: path.join(__dirname, '../../dist/browser')
+    }),
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: './data.db',
+      entities: [UserEntity],
+      synchronize: true,
+    }),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule { }
