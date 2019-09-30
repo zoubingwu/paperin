48e454cd96d89dd73ccc225094a38f60592df4e6
zoubingwu <zoubingwu@gmail.com>
2019-10-01 01:28:21 +0800

    implement users api

--- packages/server/src/modules/user/user.controller.ts
+++ packages/server/src/modules/user/user.controller.ts
@@ -1,18 +1,60 @@
-import { Controller, UseInterceptors, Get, Post, UseGuards, Body, Request } from '@nestjs/common';
-import { ApiUseTags } from '@nestjs/swagger';
+import {
+  Controller,
+  UseInterceptors,
+  Get,
+  Put,
+  Body,
+  Param,
+  Post,
+  UseGuards,
+  UsePipes,
+  ClassSerializerInterceptor,
+  NotFoundException,
+} from '@nestjs/common';
+import { ApiUseTags, ApiBearerAuth } from '@nestjs/swagger';
 import { AuthGuard } from '@nestjs/passport';
-
-import { UserService } from './user.service';
 import { LoggingInterceptor } from '../../interceptors/logger.interceptor';
-import { AuthService } from '../auth/auth.service';
+import { UserService } from './user.service';
+import { User } from './user.decorator';
+import { UserData, CreateUserDto } from './user.dto';
+import { ValidationPipe } from '../../pipes/validation.pipe';
+import { Roles } from '../roles/roles.decorator';
+import { UserRole } from './user.entity';
+import { RolesGuard } from '../roles/role.guard';
 
+@UseGuards(AuthGuard('jwt'), RolesGuard)
+@ApiBearerAuth()
 @ApiUseTags('users')
 @UseInterceptors(LoggingInterceptor)
+@UseInterceptors(ClassSerializerInterceptor)
 @Controller('users')
 export class UserController {
-  @UseGuards(AuthGuard('jwt'))
-  @Get()
-  async getUsers(@Request() req) {
-    return [1, 2, 3];
+  constructor(private readonly userService: UserService) { }
+
+  @Get('/user.me')
+  async findMe(@User('email') email: string): Promise<UserData> {
+    return await this.userService.findByEmail(email);
+  }
+
+  @Get('/:id/user.get')
+  async findOne(@Param('id') id: string) {
+    const user = await this.userService.findById(parseInt(id, 10));
+    if (!user) {
+      throw new NotFoundException('用户不存在');
+    }
+    return user;
+  }
+
+  @Roles(UserRole.ADMIN)
+  @Put('/user.update')
+  async update(@Body('id') userId: number, @Body() userData: UserData) {
+    return await this.userService.update(userId, userData);
+  }
+
+  @Roles(UserRole.ADMIN)
+  @UsePipes(new ValidationPipe())
+  @Post('/user.create')
+  async create(@Body() userData: CreateUserDto) {
+    return this.userService.create(userData);
   }
 }
--- packages/server/src/modules/user/user.service.ts
+++ packages/server/src/modules/user/user.service.ts
@@ -1,56 +1,49 @@
-import { Injectable, HttpException, HttpStatus } from '@nestjs/common';
+import { Injectable, BadRequestException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
-
+import { Repository, DeleteResult } from 'typeorm';
 import { UserEntity } from './user.entity';
-import { BaseService } from '../../base/service.base';
-import { Request } from 'express';
+import { UserData, CreateUserDto } from './user.dto';
 
 @Injectable()
-export class UserService extends BaseService<UserEntity> {
+export class UserService {
   constructor(
     @InjectRepository(UserEntity)
-    private readonly userRepository: Repository<UserEntity>
-  ) {
-    super();
-  }
+    private readonly userRepository: Repository<UserEntity>,
+  ) { }
 
   async findAll(): Promise<UserEntity[]> {
     return await this.userRepository.find();
   }
 
-  async findOneByEmail(email: string): Promise<UserEntity | null> {
-    return await this.userRepository.findOne({ email })
+  async findById(id: number): Promise<UserEntity | null> {
+    return await this.userRepository.findOne(id);
   }
 
-  async createOne(req: Request, dto: any) {
-    console.log(req);
-    console.log(dto);
-    const newUser = new UserEntity();
-    console.log(newUser)
-    if (true) {
-      this.throwBadRequestException('test');
-    }
-    return newUser;
+  async findByEmail(email: string): Promise<UserEntity | null> {
+    return await this.userRepository.findOne({ email });
   }
 
-  async create(user: any) {
-    const maybeUser = await this.findOneByEmail(user.email);
+  async create(dto: CreateUserDto) {
+    const { email } = dto;
+    const user = await this.findByEmail(email);
 
-    if (maybeUser) {
-      const errors = { username: 'Username and email must be unique.' };
-      throw new HttpException({
-        message: 'Input data validation failed',
-        errors,
-      }, HttpStatus.BAD_REQUEST);
+    if (user) {
+      throw new BadRequestException('邮箱已被占用');
     }
 
-    const newUser = new UserEntity();
-
-    // TODO validate input
-
-    this.userRepository.save(newUser);
-    return newUser;
+    const newUser = new UserEntity(dto);
+    return await this.userRepository.save(newUser);
   }
 
+  async update(id: number, dto: Partial<UserData>): Promise<UserEntity> {
+    const toUpdate = await this.userRepository.findOne(id);
+    delete toUpdate.password;
+
+    const updated = Object.assign(toUpdate, dto);
+    return await this.userRepository.save(updated);
+  }
+
+  async delete(email: string): Promise<DeleteResult> {
+    return await this.userRepository.delete({ email });
+  }
 }
