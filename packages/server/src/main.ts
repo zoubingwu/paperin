516b084e14540e9f9a57a792aa44c70c6d1610f9
zoubingwu <zoubingwu@gmail.com>
2019-09-30 20:09:31 +0800

    disable x-powered-by express

--- packages/server/src/main.ts
+++ packages/server/src/main.ts
@@ -1,12 +1,14 @@
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { logger } from './middlewares/logger.middleware';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.setGlobalPrefix('api');
   app.use(logger);
+  app.disable('x-powered-by');
 
   const options = new DocumentBuilder()
     .setTitle('Paperin API documentation')
