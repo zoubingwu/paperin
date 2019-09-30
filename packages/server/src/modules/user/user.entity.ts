598cf755d0c5dc77fe22bbfd7669a0992b1e59ec
zoubingwu <zoubingwu@gmail.com>
2019-09-30 23:28:33 +0800

    update user entity

--- packages/server/src/modules/user/user.entity.ts
+++ packages/server/src/modules/user/user.entity.ts
@@ -1,53 +1,64 @@
-import { Entity, PrimaryGeneratedColumn, Column, BeforeInsert, CreateDateColumn, UpdateDateColumn } from 'typeorm';
-import { IsEmail } from 'class-validator';
+import { Entity, Column, BeforeInsert } from 'typeorm';
+import { IsEnum } from 'class-validator';
+import { Exclude } from 'class-transformer';
 import { hashPassword } from '../../utils/hashPassword';
 import { BaseEntity } from '../../base/entity.base';
 
 export enum UserRole {
   ADMIN = 'admin',
   NORMAL = 'normal',
+}
 
+export enum Sex {
+  MALE = 'male',
+  FEMALE = 'female',
 }
 
 @Entity('user')
 export class UserEntity extends BaseEntity {
+  constructor(partial: Partial<UserEntity>) {
+    super();
+    Object.assign(this, partial);
+  }
+
+  @Column({ length: 50, unique: true })
+  email: string;
+
   @Column({ length: 50 })
   name: string;
 
-  @Column({ length: 10 })
-  sex: string;
-
-  @Column({ length: 10 })
-  ethnic: string;
-
-  @Column({ length: 20 })
-  phone: string;
-
-  @Column({ length: 50, unique: true })
-  @IsEmail()
-  email: string;
-
-  @Column({ length: 100 })
-  address: string;
-
-  @Column({ type: 'datetime' })
-  birthday: Date;
-
-  @Column({ length: 10 })
-  degree: string;
-
-  @Column({ length: 10 })
-  department: string;
-
-  @Column({})
-  work_id: string;
+  @IsEnum(Sex)
+  @Column({ length: 10, default: Sex.MALE })
+  sex: Sex;
 
   @Column({ length: 10, default: UserRole.NORMAL })
   role: UserRole;
 
-  @Column({ type: 'datetime' })
-  begin_work_at: Date;
+  @Column({ length: 10, nullable: true })
+  ethnic?: string;
 
+  @Column({ length: 20, nullable: true })
+  phone?: string;
+
+  @Column({ length: 100, nullable: true })
+  address?: string;
+
+  @Column({ nullable: true })
+  birthday?: number;
+
+  @Column({ length: 10, nullable: true })
+  degree?: string;
+
+  @Column({ length: 10, nullable: true })
+  department?: string;
+
+  @Column({ nullable: true })
+  workId?: string;
+
+  @Column({ nullable: true })
+  beginWorkAt?: number;
+
+  @Exclude()
   @Column({ length: 100 })
   password: string;
 
