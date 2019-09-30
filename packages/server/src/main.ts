bb17450552e03101aeeb94b90c3aac63a7902d32
zoubingwu <zoubingwu@gmail.com>
2019-10-01 01:23:16 +0800

    remove csurf

--- packages/server/src/main.ts
+++ packages/server/src/main.ts
@@ -2,18 +2,20 @@ import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import * as helmet from 'helmet';
-import * as csurf from 'csurf';
 import * as rateLimit from 'express-rate-limit';
+import * as dotenv from 'dotenv';
+import * as path from 'path';
 
 import { AppModule } from './app.module';
 import { logger } from './middlewares/logger.middleware';
+
+dotenv.config({ path: path.join(process.cwd(), `packages/server/config/${process.env.NODE_ENV}.env`) });
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.setGlobalPrefix('api');
   app.use(logger);
   app.use(helmet());
-  app.use(csurf());
   app.use(rateLimit({
     windowMs: 15 * 60 * 1000, // 15 minutes
     max: 100, // limit each IP to 100 requests per windowMs
