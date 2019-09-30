04a7a51b407b4e3d989219c5e0e1ce906f83fbe0
zoubingwu <zoubingwu@gmail.com>
2019-10-01 01:24:16 +0800

    add role in jwt token

--- packages/server/src/modules/auth/auth.service.ts
+++ packages/server/src/modules/auth/auth.service.ts
@@ -2,29 +2,29 @@ import { Injectable } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { hashPassword } from '../../utils/hashPassword';
 import { UserService } from '../user/user.service';
+import { UserData } from '../user/user.dto';
 
 @Injectable()
 export class AuthService {
   constructor(
     private readonly userService: UserService,
-    private readonly jwtService: JwtService
+    private readonly jwtService: JwtService,
   ) { }
 
-  async validateUser(email, password) {
-    return { email, id: 1 };
-    const user = await this.userService.findOneByEmail(email);
+  async validateUser(email: string, password: string) {
+    const user = await this.userService.findByEmail(email);
     if (user && user.password === hashPassword(password)) {
       return user;
     }
     return null;
   }
 
-  async login(user: any) {
-    const payload = { email: user.email, sub: user.id };
+  async login(user: Partial<UserData>) {
+    const payload = { email: user.email, sub: user.id, role: user.role };
     return {
-      email: user.email,
       id: user.id,
-      access_token: this.jwtService.sign(payload)
-    }
+      email: user.email,
+      access_token: this.jwtService.sign(payload),
+    };
   }
 }
