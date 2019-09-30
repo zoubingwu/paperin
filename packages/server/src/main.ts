2efcd6fdca6f2c3862a18e901fb03fe5fe55da42
zoubingwu <zoubingwu@gmail.com>
2019-09-30 23:00:59 +0800

    improve security

--- packages/server/src/main.ts
+++ packages/server/src/main.ts
@@ -1,6 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import * as helmet from 'helmet';
+import * as csurf from 'csurf';
+import * as rateLimit from 'express-rate-limit';
+
 import { AppModule } from './app.module';
 import { logger } from './middlewares/logger.middleware';
 
@@ -8,7 +12,12 @@ async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.setGlobalPrefix('api');
   app.use(logger);
-  app.disable('x-powered-by');
+  app.use(helmet());
+  app.use(csurf());
+  app.use(rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100, // limit each IP to 100 requests per windowMs
+  }));
 
   const options = new DocumentBuilder()
     .setTitle('Paperin API documentation')
@@ -21,4 +30,5 @@ async function bootstrap() {
 
   await app.listen(3000);
 }
+
 bootstrap();
