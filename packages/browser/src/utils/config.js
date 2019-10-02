972a566114713e41d320db765de8d7adf2802936
zoubingwu <zoubingwu@gmail.com>
2019-10-02 22:46:37 +0800

    add utils

---
+++ packages/browser/src/utils/config.js
@@ -0,0 +1,34 @@
+module.exports = {
+  siteName: 'Human Resources',
+  copyright: 'Human Resources  ©2019 zoubingwu',
+  logoPath: '/logo.svg',
+  apiPrefix: '/api/v1',
+  fixedHeader: true, // sticky primary layout header
+
+  /* Layout configuration, specify which layout to use for route. */
+  layouts: [
+    {
+      name: 'primary',
+      include: [/.*/],
+      exclude: [/(\/(en|zh))*\/login/],
+    },
+  ],
+
+  /* I18n configuration, `languages` and `defaultLanguage` are required currently. */
+  i18n: {
+    /* Countrys flags: https://www.flaticon.com/packs/countrys-flags */
+    languages: [
+      {
+        key: 'en',
+        title: 'English',
+        flag: '/america.svg',
+      },
+      {
+        key: 'zh',
+        title: '中文',
+        flag: '/china.svg',
+      },
+    ],
+    defaultLanguage: 'en',
+  },
+};
